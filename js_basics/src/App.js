98999dbdf69926d532dd181e91a9a64e6d8ffda1
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-09-28 15:16:40 -0300

    React_4_JS_Basics Parte 2: Mudanças e ajustes

--- js_basics/src/App.js
+++ js_basics/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//JS Basics Parte 1:
+
+//var
+var variavel1 = 10;
+console.log(variavel1);
+
+//let
+let variavel2 = 10;
+console.log(variavel2);
+
+let variavel3 = 10;
+console.log(variavel3);
+variavel3 = variavel3 + 3;
+console.log(variavel3);
+
+//const
+const variavel4 = 10;
+console.log(variavel4);
+
+const soma2 = function soma2(a, b) {
+  return a + b;
+};
+console.log(soma2(1, 3));
+
+//function
+const funcSoma = soma;
+console.log(funcSoma(4, 5));
+
+console.log(soma(1, 2));
+function soma(c, d) {
+  return c + d;
 }
-
-export default App;
--- js_basics/src/index.js
+++ js_basics/src/index.js
@@ -1,50 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-//JS Basics Parte 1:
-
-//var
-var variavel1 = 10;
-console.log(variavel1);
-
-//let
-let variavel2 = 10;
-console.log(variavel2);
-
-let variavel3 = 10;
-console.log(variavel3);
-variavel3 = variavel3 + 3;
-console.log(variavel3);
-
-//const
-const variavel4 = 10;
-console.log(variavel4);
-
-const soma2 = function soma2(a, b) {
-  return a + b;
-};
-console.log(soma2(1, 3));
-
-//function
-const funcSoma = soma;
-console.log(funcSoma(4, 5));
-
-console.log(soma(1, 2));
-function soma(c, d) {
-  return c + d;
-}
