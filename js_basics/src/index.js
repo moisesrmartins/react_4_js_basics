ae5966991a84bde86bdbfa40af5b6a0a766824b1
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-09-28 17:47:41 -0300

    React_4_JS_Basics Parte 4: Mudanças e ajustes 2

--- js_basics/src/index.js
+++ js_basics/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import Basics from "./Basics";
 import reportWebVitals from "./reportWebVitals";
 
 const rootElement = document.getElementById("root");
@@ -9,6 +10,7 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
+    <Basics />
   </StrictMode>
 );
 // If you want to start measuring performance in your app, pass a function
