e5a6c5144942d6dfe5530ce9e716278556089f17
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-09-29 17:57:10 -0300

    React_4_JS_Basics Parte 8.2: Components2

--- js_basics/src/Components.js
+++ js_basics/src/Components.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+class Components2 extends React.Component {
+  render() {
+    return <h3>Nice to meet you</h3>;
+  }
+}
+
 function Components() {
-  return <div className="Components"></div>;
+  return (
+    <div className="Components">
+      <Components2 />
+    </div>
+  );
 }
 
 export default Components;
