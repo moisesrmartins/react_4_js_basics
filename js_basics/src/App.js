5dc4cbb0702f0a1d340ccbb3b48aeb8e7b776684
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-09-29 10:03:24 -0300

    React_4_JS_Basics Parte 7.1: Import Header

--- js_basics/src/App.js
+++ js_basics/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import Header from "./Header";
 
 const App = () => {
   return (
     <div className="App">
       <div>
         <h1>START</h1>
+      </div>
+
+      <div>
+        <Header />
       </div>
     </div>
   );
---
+++ js_basics/src/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => (
+  <p>
+    <b>Hi</b>
+  </p>
+);
+
+export default Header;
